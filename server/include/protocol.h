4995c1893d755ce6bad4d0f18459aa469a3aff9a
gtxzsxxk <2524395907@qq.com>
2024-10-09 21:14:26 +0800

    server: feat: add basic template for client state machine

---
+++ server/include/protocol.h
@@ -0,0 +1,32 @@
+//
+// Created by hanyuan on 2024/10/9.
+//
+
+#ifndef SERVER_PROTOCOL_H
+#define SERVER_PROTOCOL_H
+
+#define MAX_CLIENTS 100
+
+enum ftp_state_machine {
+    IDLE,
+    NEED_SEND,
+};
+
+struct client_data {
+    int sock_fd;
+    int nfds;
+    enum ftp_state_machine state;
+
+    /* 解决粘包，根据CRLF进行切分 */
+    int recv_ptr;
+    char cmd_request_buffer[512];
+    char cmd_send[512];
+};
+
+struct client_data *protocol_client_init(int fd, int nfds);
+
+void protocol_client_free(int fd);
+
+struct client_data *protocol_client_by_fd(int fd);
+
+#endif //SERVER_PROTOCOL_H
---
+++ server/src/protocol.c
@@ -0,0 +1,50 @@
+//
+// Created by hanyuan on 2024/10/9.
+//
+
+#include "../include/protocol.h"
+#include <string.h>
+
+const char *BANNER_STRING = "Welcome to Friendly Young Zero FTP Server!";
+
+static struct client_data clients[MAX_CLIENTS] = {0};
+
+static int protocol_client_index_by_fd(int fd) {
+    /* TODO: 性能关键路径，用 hash */
+    int latest_free = -1;
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].sock_fd == fd) {
+            return i;
+        } else if (clients[i].sock_fd == 0 && latest_free == -1) {
+            latest_free = i;
+        }
+    }
+
+    return latest_free;
+}
+
+struct client_data *protocol_client_init(int fd, int nfds) {
+    /* 分配一个新的 client_data */
+    int index = protocol_client_index_by_fd(fd);
+    clients[index].sock_fd = fd;
+    clients[index].state = NEED_SEND;
+    clients[index].nfds = nfds;
+    strcpy(&(clients[index].cmd_send[0]), BANNER_STRING);
+
+    return &clients[index];
+}
+
+void protocol_client_free(int fd) {
+    int index = protocol_client_index_by_fd(fd);
+    clients[index].sock_fd = 0;
+}
+
+struct client_data *protocol_client_by_fd(int fd) {
+    /* 分配一个新的 client_data */
+    int index = protocol_client_index_by_fd(fd);
+    if (!clients[index].sock_fd) {
+        return NULL;
+    }
+
+    return &clients[index];
+}
