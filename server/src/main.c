5b6ff7110a539522213b637be83eccf8390a711c
Hanyuan Zhao <hanyuan-z@qq.com>
2024-10-09 11:37:29 +0800

    server: feat: add argument parsing

---
+++ server/src/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void usage(void) {
+    printf("%s", "server error: bad arguments\n\n");
+    printf("%s", "Usage:\n");
+    printf("%s", "server -port 11341 -root /root/ftp/\n");
+}
+
+int main(int argc, char **argv) {
+    int assigned_port = 0, assigned_root = 0;
+    int port = 0;
+    char root_path[256];
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-port")) {
+            if (i + 1 < argc) {
+                assigned_port = 1;
+                port = atoi(argv[i + 1]);
+                if (port == 0) {
+                    printf("server error: illegal port %d\n", port);
+                    return 1;
+                }
+                i++;
+            } else {
+                usage();
+                return 1;
+            }
+        } else if (!strcmp(argv[i], "-root")) {
+            if (i + 1 < argc) {
+                assigned_root = 1;
+                strcpy(root_path, argv[i + 1]);
+                i++;
+            } else {
+                usage();
+                return 1;
+            }
+        } else {
+            usage();
+            return 1;
+        }
+    }
+    if (!assigned_port || !assigned_root) {
+        usage();
+        return 1;
+    }
+
+    printf("server info: listening on port %d\n", port);
+    printf("server info: root filesystem %s\n", root_path);
+    return 0;
+}
